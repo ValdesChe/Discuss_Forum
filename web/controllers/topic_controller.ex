7a1761025e176bd88ab48553971c970cb1499093
ValdoR Che <ingeekteam@gmail.com>
2018-10-30 07:38:32 +0000

    Index func in topic_controller for listing topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -21,4 +21,16 @@ defmodule Discuss.TopicController do
         render(conn, "new.html", changeset: changeset)
     end
   end
+
+  def index(conn, _params) do
+    changeset = Discuss.Topic.changeset(%Discuss.Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        IO.inspect(post)
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
 end
