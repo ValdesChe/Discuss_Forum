20a6e338ec6491b3d51fee3572e504824ecc979a
ValdoR Che <ingeekteam@gmail.com>
2018-10-31 14:22:15 +0100

    Config ueberauth in config/config.exs file for our client_id and client_secret keys

--- config/config.exs
+++ config/config.exs
@@ -14,8 +14,7 @@ config :discuss, Discuss.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "7m71s0PpicVZGncI1nRjVswobhFiaUanAt5lDKBXCK6yBKJKJJupH/obzeP1DotU",
   render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +23,9 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
