6fe4143ff430634d1e13b9d7004ef3668eb5cc66
ValdoR Che <ingeekteam@gmail.com>
2018-11-03 00:10:42 +0100

    [Discuss - Plug ] Modifying our conn struct and setting user in assign prop .

--- web/controllers/plugs/set_user.ex
+++ web/controllers/plugs/set_user.ex
@@ -4,7 +4,7 @@ defmodule Discuss.Plugs.SetUser do
 
   alias Discuss.Repo
   alias Discuss.User
-  alias Discuss.Router.Helpers
+  # alias Discuss.Router.Helpers
 
   @doc """
       Called one time.
@@ -19,5 +19,19 @@ defmodule Discuss.Plugs.SetUser do
       `params` is value returned by `Init` function.
   """
   def call(conn, _params) do
+    # We retrieve the user's info from session store
+    user_id = get_session(conn, :user_id)
+
+    # The assign function used here take a connection, modify the assign prop
+    # and then return a connection ( conn )
+    cond do
+      # Return the user from database if the user_id is defined
+      user = user_id && Repo.get(User, user_id) ->
+        # Modify our assigns prop in the conn Struct
+        assign(conn, :user, user)
+
+      true ->
+        assign(conn, :user, nil)
+    end
   end
 end
