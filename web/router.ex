7321103046ccd0e61e83718af10d4b1e78fa2830
ValdoR Che <ingeekteam@gmail.com>
2018-11-03 00:28:58 +0100

    Adding our module plug in route for browser request and specifying in the app.html.eex layout the logout and Sign in wih github option.

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,9 @@ defmodule Discuss.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+
+    # Adding our module plug for our browser's request
+    plug(Discuss.Plugs.SetUser)
   end
 
   pipeline :api do
