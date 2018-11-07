6afd8fbf459d649069425115927d43323c06f00f
ValdoR Che <ingeekteam@gmail.com>
2018-11-07 20:46:36 +0100

    Associating comments with Users.

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -22,10 +22,11 @@ defmodule Discuss.CommentsChannel do
 
   def handle_in(name, %{"content" => content}, socket) do
     topic = socket.assigns[:topic]
+    user_id = socket.assigns.user_id
 
     changeset =
       topic
-      |> build_assoc(:comments)
+      |> build_assoc(:comments, user_id: user_id)
       |> Comment.changeset(%{content: content})
 
     case Repo.insert(changeset) do
--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -21,8 +21,14 @@ defmodule Discuss.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _reason} ->
+        :error
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
